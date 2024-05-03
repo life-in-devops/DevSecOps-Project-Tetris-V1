b77838d3037298411fdadc396ee4276a7e58b5ee
narasimha-epi <narasimha.jetty@extrapreneursindia.com>
2024-05-03 13:34:24 +0530

    updated EKS terraform script for backend

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -34,7 +34,7 @@ data "aws_subnets" "public" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
+  name     = "DevSecOps-Project2"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
 #create node group
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "Node-cloud"
+  node_group_name = "DevSecOps-Project2-node-group"
   node_role_arn   = aws_iam_role.example1.arn
   subnet_ids      = data.aws_subnets.public.ids
 
@@ -90,7 +90,7 @@ resource "aws_eks_node_group" "example" {
     max_size     = 2
     min_size     = 1
   }
-  instance_types = ["t2.medium"]
+  instance_types = ["t3.medium"]
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
