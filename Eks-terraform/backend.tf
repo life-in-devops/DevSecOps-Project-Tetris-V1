c362759104e327a1787025e39ee2ae6c313b284a
Narasimha Jetty <129029918+life-in-devops@users.noreply.github.com>
2024-05-04 12:03:43 +0530

    Updated backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devops-project-terraform-backups" # Replace with your actual S3 bucket name
+    bucket = "venkatesh790" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
