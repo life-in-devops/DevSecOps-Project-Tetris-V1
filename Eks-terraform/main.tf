73213a2dfecaf28c188933c0cc110bfb4a81f982
Narasimha Jetty <129029918+life-in-devops@users.noreply.github.com>
2024-05-07 06:05:14 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups etc.
 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
